ec79f4c41e96ab66bbd404dff226e550c204f034
Takenori Okada <okajax@gmail.com>
2016-09-01 00:13:11 +0900

    test waterfall

--- app.js
+++ app.js
@@ -23,9 +23,25 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', function (session) {
-    session.send("Hello World");
-});
+bot.dialog('/', [
+    function (session) {
+        builder.Prompts.text(session, "Hello... What's your name?");
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been coding?");
+    },
+    function (session, results) {
+        session.userData.coding = results.response;
+        builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
+    },
+    function (session, results) {
+        session.userData.language = results.response.entity;
+        session.send("Got it... " + session.userData.name +
+                     " you've been programming for " + session.userData.coding +
+                     " years and use " + session.userData.language + ".");
+    }
+]);
 
 // Serve a static web page
 // Webブラウザでアクセスされた時には、静的HTMLを表示させる
